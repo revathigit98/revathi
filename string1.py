750172cf080fcbe6b6763e7822e49f63df11d505
revathigit98 <60249969+revathigit98@users.noreply.github.com>
2020-01-24 11:58:00 +0530

    Add files via upload

---
+++ string1.py
@@ -0,0 +1,6 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:21:23) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=" hello world "
+>>> print(a.strip())
+hello world
+>>> 
